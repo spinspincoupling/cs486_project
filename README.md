6b043c0706376cca0280419c3b2b0b0b8c88cadc
Zhaoqi Wang <z833wang@uwaterloo.ca>
2021-06-12 20:45:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cs486_project
---
+++ train.py
@@ -0,0 +1,15 @@
+'''
+This script handles the training process.
+'''
+
+
+def train():
+    print("Start training...")
+
+
+def main():
+    train()
+
+
+if __name__ == '__main__':
+    main()
