653e486a524f767365560c1580d0a6ab74159b47
Zhaoqi Wang <z833wang@uwaterloo.ca>
2021-06-24 17:05:36 -0400

    implement decoder

--- parameters.py
+++ parameters.py
@@ -2,18 +2,19 @@
 d_k = 64
 d_v = 64
 d_ff = 2048
-d_model = 512
+d_embedding = 512
 num_head = 8
 num_stacks = 4
 encoder = 'resnet34'
+num_frames = 103
 
 LEARNING_RATE = 0.0001
 TRAIN_STEPS = 1000
 BATCH_SIZE = 1
 
-SEQ_SIZE = 16
+SEQ_SIZE = 103
 STRIDE = 2*SEQ_SIZE
 IN_DEPTH = 2*SEQ_SIZE
-IN_HEIGHT = 224
-IN_WIDTH = 224
+IN_HEIGHT = 240
+IN_WIDTH = 320
 IN_CHANNEL = 3
--- transformer/VTN.py
+++ transformer/VTN.py
@@ -1,7 +1,6 @@
 # This implements the VTN AQA model
 import torch
 import torch.nn as nn
-from torch import optim
 import torch.nn.functional as F
 import encoder
 import decoder
@@ -13,13 +12,19 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class VTN(nn.Module):
     def __init__(self):
         super(VTN, self).__init__()
-        self.encoder = encoder()
-        self.decoder = decoder()
+        self.encoder = encoder.Encoder()
+        self.decoder = decoder.AttnDecoder()
         self.fc = nn.nn.Linear(d_ff, 1)
 
     def forward(self, clips, difficulty_levels):
         output = self.encoder.forward(clips)
         for i in range(num_stacks):
-            output = self.decoder.forward(output)
+            output = self.decoder.forward(output, output, output)
         output = self.fc(torch.cat(output, difficulty_levels, 1))
         return output
+
+
+def test():
+    model = VTN()
+    for name, param in model.named_parameters():
+        print(name, param.size())
