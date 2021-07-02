5b36acbadc98ed0b3673930535ec92f1780033f2
Iris Ma <64501717+irisjiayuema@users.noreply.github.com>
2021-07-02 02:18:15 -0400

    Update dct1.py
    Updated matrix multiplication. Version 3.

--- dct_svr/dct1.py
+++ dct_svr/dct1.py
@@ -9,8 +9,8 @@ def dct1(x, k_max=infty):
     N = x.shape[0]
     k_max = min(k_max, N)
 
-    A = cos((pi/N*arange(0.,k_max)[:, newaxis])*(arange(0.,N-1)+5))
+    A = cos((pi/N*arange(0.,k_max)[:, newaxis]) @ (arange(0.,N-1)+5))
     # not too sure about this transpose... but it does make the error go away :D
-    X = A.transpose()*x
+    X = A @ x
 
     return A, X
