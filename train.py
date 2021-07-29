5c61f8e516bfa4ba9cc8c70d64d418c01a512b20
Zhaoqi Wang <z833wang@uwaterloo.ca>
2021-07-29 16:17:40 -0400

    move data

--- train.py
+++ train.py
@@ -109,6 +109,7 @@ def main():
             # print("trainingData:", trainingData.shape)
             # print("trainingDifficultyLevels:", trainingDifficultyLevels.size())
             # print("trainingOverallScores:", trainingOverallScores.size())
+            trainingData, trainingDifficultyLevels, trainingOverallScores = trainingData.to(device), trainingDifficultyLevels.to(device), trainingOverallScores.to(device)
             vtn, loss = trainOnData(vtn, optimizer, trainingData, trainingDifficultyLevels, trainingOverallScores)
             epochTrainError.append(loss)
             if loss < best:
